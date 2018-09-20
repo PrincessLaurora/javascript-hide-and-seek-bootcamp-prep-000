f95ac55f427176588ee52b09c718f3672cafff7f
Laura Felce <crepu83@yahoo.it>
2018-09-20 14:38:51 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+function getFirstSelector(selector) {
+return document.querySelector(selector);
+}
 
+function nestedTarget() {
+  return document.getElementById('nested').querySelector('div.target');
+}
+function deepestChild() {
+ var element = document.getElementById('grand-node');
+ var child = element.children[0];
+ while (child) {
+   element = child;
+   child = element.children[0];
+ } return element;
+}
+   
+   function increaseRankBy(n) {
+    const ranghi = document.getElementById("app")
+  .querySelectorAll("ul.ranked-list li");
+  for (let i = 0; i < ranghi.length; i++) {
+   ranghi[i].innerHTML = parseInt(ranghi[i].innerHTML) + n
+    }
+   }
