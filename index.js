cf40253760914ba1820dd1db0d0db804f5fc914d
Laura Felce <crepu83@yahoo.it>
2018-09-20 13:22:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,4 +6,4 @@ function nestedTarget() {
   return document.getElementById('nested').querySelector('div.target');
 }
 function deepestChild() {
- const child =  document.getElementById ('grand-node').querySelectorAll('')
+ const child =  document.getElementById ('grand-node').querySelectorAll('div')
